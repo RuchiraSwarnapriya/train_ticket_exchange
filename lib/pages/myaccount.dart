bbc1ec9df699ba4dee70e7cc039e45d659b38e32
Ruchira Swarnapriya <ruchira.swarnapriya8@gmail.com>
2019-05-03 22:35:17 +0530

    my account dashboard

--- lib/pages/myaccount.dart
+++ lib/pages/myaccount.dart
@@ -15,22 +15,55 @@ class _MyAccountState extends State<MyAccount> {
         backgroundColor: Colors.transparent,
         iconTheme: new IconThemeData(color:Colors.black),
       ),
-      body: Center(
-        child: Container(
-          width: 200.0,
-          height: 200.0,
-          decoration: BoxDecoration(
-            image: DecorationImage(image: AssetImage('images/myaccount/sell.png')
-            ),
-          ),
-          child: FlatButton(
-            padding: EdgeInsets.all(0.0),
-              onPressed: ()=> Navigator.push(context,new MaterialPageRoute(builder: (context)=> AddNewTicket())),
-              child: null
-          ),
+      body:Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 3.0),
+        child: GridView.count(
+          crossAxisCount: 2,
+          padding: EdgeInsets.all(3.0),
+          children: <Widget>[
+            makeDashboardItem("Ordbog"),
+//            makeDashboardItem("Alphabet", Icons.monetization_on),
+//            makeDashboardItem("Alphabet", Icons.alarm),
+//            makeDashboardItem("Alphabet", Icons.alarm),
+//            makeDashboardItem("Alphabet", Icons.alarm),
+//            makeDashboardItem("Alphabet", Icons.alarm)
+          ],
+
         ),
-      ),
+      )
 
     );
   }
+  Card makeDashboardItem(String title, ) {
+    return Card(
+        elevation: 1.0,
+        margin: new EdgeInsets.all(8.0),
+        child: Container(
+          decoration: BoxDecoration(color: Color.fromRGBO(220, 220, 220, 1.0)),
+          child: new InkWell(
+            onTap: () {},
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.min,
+              verticalDirection: VerticalDirection.down,
+              children: <Widget>[
+                SizedBox(height: 50.0),
+//                Center(
+//                    child: Icon(
+//                      icon,
+//                      size: 40.0,
+//                      color: Colors.black,
+//                    )),
+                SizedBox(height: 20.0),
+                new Center(
+                  child: new Text(title,
+                      style:
+                      new TextStyle(fontSize: 18.0, color: Colors.black)),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+
 }
