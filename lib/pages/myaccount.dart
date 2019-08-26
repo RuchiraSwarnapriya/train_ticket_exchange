9a6e2e6fb9877f3aeb05f49d7c977543aa2f6594
RuchiraSwarnapriya <ruchira.swarnapriya8@gmail.com>
2019-08-26 13:23:17 +0530

    Request Ticket Page

--- lib/pages/myaccount.dart
+++ lib/pages/myaccount.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:train_ticket_exchange/pages/addnewticket.dart';
 import 'package:train_ticket_exchange/pages/settings.dart';
+import 'package:train_ticket_exchange/pages/requestTicket.dart';
 
 class MyAccount extends StatefulWidget {
   @override
@@ -20,16 +21,18 @@ class _MyAccountState extends State<MyAccount> {
 
       body: StaggeredGridView.count(
         crossAxisCount: 2,
-        crossAxisSpacing: 12.0,
-        mainAxisSpacing: 12.0,
-        padding: EdgeInsets.symmetric(horizontal: 16.0,vertical: 8.0),
+        crossAxisSpacing: 10.0,
+        mainAxisSpacing: 10.0,
+        padding: EdgeInsets.symmetric(horizontal: 8.0,vertical: 8.0),
         children: <Widget>[
-          myItems(Icons.attach_money,"Sell Ticket"),
-          myItems(Icons.settings,"Settings"),
+          sellTickets(Icons.attach_money,"Sell Ticket"),
+          requestTickets(Icons.train,"Request Ticket"),
+
+
         ],
         staggeredTiles: [
-          StaggeredTile.extent(1, 150.0),
-          StaggeredTile.extent(1, 150.0),
+          StaggeredTile.extent(1, 130.0),
+          StaggeredTile.extent(1, 130.0),
         ],
 
       ),
@@ -38,7 +41,60 @@ class _MyAccountState extends State<MyAccount> {
     );
   }
 
-  Material myItems(IconData icon,String heading){
+//  Material settings(IconData icon,String heading){
+//    return Material(
+//      color: Colors.white,
+//      elevation: 14.0,
+//      shadowColor: Colors.grey,
+//      borderRadius: BorderRadius.circular(15.0),
+//      child: Center(
+//        child: Padding(
+//          padding: const EdgeInsets.all(8.0),
+//          child: new InkWell(
+//            onTap: () =>
+//                Navigator.push(context, new MaterialPageRoute(
+//                    builder: (context) => Settings())),
+//            child: Row(
+//              mainAxisAlignment: MainAxisAlignment.center,
+//              children: <Widget>[
+//                Column(
+//                  mainAxisAlignment: MainAxisAlignment.center,
+//                  children: <Widget>[
+//                    Padding(
+//                      padding: const EdgeInsets.all(8.0),
+//                      child: Text(heading,
+//                        style: TextStyle(
+//                          fontSize: 20.0,
+//                        ),
+//                      ),
+//                    ),
+//
+//                    Material(
+////                      color: Colors.black,
+////                      borderRadius: BorderRadius.circular(10.0),
+//                      child: Padding(
+//                        padding: const EdgeInsets.all(16.0),
+//                        child: Icon(
+//                          icon,
+//                          color: Colors.black,
+//                          size: 30.0,
+//                        ),
+//
+//                      ),
+//                    ),
+//                  ],
+//                )
+//              ],
+//            ),
+//
+//          )
+//
+//        ),
+//      ),
+//    );
+//  }
+
+  Material sellTickets(IconData icon,String heading){
     return Material(
       color: Colors.white,
       elevation: 14.0,
@@ -46,50 +102,104 @@ class _MyAccountState extends State<MyAccount> {
       borderRadius: BorderRadius.circular(15.0),
       child: Center(
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: new InkWell(
-            onTap: () =>
-                Navigator.push(context, new MaterialPageRoute(
-                    builder: (context) => AddNewTicket())),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(heading,
-                        style: TextStyle(
-                          fontSize: 20.0,
+            padding: const EdgeInsets.all(8.0),
+            child: new InkWell(
+              onTap: () =>
+                  Navigator.push(context, new MaterialPageRoute(
+                      builder: (context) => AddNewTicket())),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(heading,
+                          style: TextStyle(
+                            fontSize: 20.0,
+                          ),
                         ),
                       ),
-                    ),
 
-                    Material(
+                      Material(
 //                      color: Colors.black,
 //                      borderRadius: BorderRadius.circular(10.0),
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Icon(
-                          icon,
-                          color: Colors.black,
-                          size: 30.0,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Icon(
+                            icon,
+                            color: Colors.black,
+                            size: 30.0,
+                          ),
+
                         ),
-
                       ),
-                    ),
-                  ],
-                )
-              ],
-            ),
+                    ],
+                  )
+                ],
+              ),
 
-          )
+            )
 
         ),
       ),
     );
   }
+
+  Material requestTickets(IconData icon,String heading){
+    return Material(
+      color: Colors.white,
+      elevation: 14.0,
+      shadowColor: Colors.grey,
+      borderRadius: BorderRadius.circular(15.0),
+      child: Center(
+        child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: new InkWell(
+              onTap: () =>
+                  Navigator.push(context, new MaterialPageRoute(
+                      builder: (context) => RequestTicket())),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(heading,
+                          style: TextStyle(
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
+
+                      Material(
+//                      color: Colors.black,
+//                      borderRadius: BorderRadius.circular(10.0),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Icon(
+                            icon,
+                            color: Colors.black,
+                            size: 30.0,
+                          ),
+
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+
+            )
+
+        ),
+      ),
+    );
+  }
+
 
 
 }
