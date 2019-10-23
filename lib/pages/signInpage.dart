5742b489db93303897018bc2e04df956e39ef71b
Ruchira Bandara <ruchira.swarnapriya8@gmail.com>
2019-10-23 09:51:47 +0530

    Change Home Screen

--- lib/pages/signInpage.dart
+++ lib/pages/signInpage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:train_ticket_exchange/pages/myaccount.dart';
 import 'package:train_ticket_exchange/pages/signUppage.dart';
-import 'package:train_ticket_exchange/pages/ticketdetails.dart';
 import 'package:train_ticket_exchange/pages/addnewticket.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
